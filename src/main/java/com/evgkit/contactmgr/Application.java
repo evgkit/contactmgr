c0e4d6e830c94ff0eb7a6ce261009eb880525619
evgkit <kitaev.ea@gmail.com>
2017-08-07 17:05:07 +0300

    Comprehensive commit

--- src/main/java/com/evgkit/contactmgr/Application.java
+++ src/main/java/com/evgkit/contactmgr/Application.java
@@ -1,10 +1,14 @@
 package com.evgkit.contactmgr;
 
 import com.evgkit.contactmgr.model.Contact;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class Application {
     private static final SessionFactory sessionFactory = buildSessionFactory();
@@ -18,6 +22,33 @@ public class Application {
         Contact contact = new Contact.ContactBuilder("Dude", "Some")
                 .withEmail("example@example.com")
                 .build();
-        System.out.println(contact);
+
+        save(contact);
+
+        for (Contact fetchedContact : fetchAllContacts()) {
+            System.out.println(fetchedContact);
+        }
+    }
+
+    private static void save(Contact contact) {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(contact);
+
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts() {
+        Session session = sessionFactory.openSession();
+
+        Criteria criteria = session.createCriteria(Contact.class);
+        List<Contact> contacts = criteria.list();
+
+        session.close();
+
+        return contacts;
     }
 }
